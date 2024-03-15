e9f7d68b28c903027e3078786fd4dcae541eb23f
Farsait <158764896+Farsait@users.noreply.github.com>
2024-03-15 22:42:04 +0400

    Update PolyLine.java

--- src/main/java/Gor/PolyLine.java
+++ src/main/java/Gor/PolyLine.java
@@ -7,22 +7,23 @@ public class PolyLine {
     public PolyLine(Point... points){
         this.points=points;
     }
-    public Line[] lineArray(){
+    public Line[] tolineArray(){
         Line[]arr=new Line[points.length-1];
         for (int i=1; i< points.length; i++){
             arr[i-1]=new Line(points[i-1],points[i]);
         }
         return arr;
     }
-    public double lenght(){
-        Line[] lines=lineArray();
+      public double lenght(){
+        Line[] lines=tolineArray();
         double sum=0;
         for (int i=0; i< lines.length;i++){
             sum+=lines[i].lenght();
         }
         return sum;
     }
-    public String prop(){
+    public String toString(){
+
         return "Line{" + Arrays.toString(points) + '}';
     }
 
